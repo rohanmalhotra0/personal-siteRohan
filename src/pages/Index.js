f8cfcf938a8fd55db6d3028447899d0a2e629dee
Rohan Malhotra <104740676+rohanmalhotra0@users.noreply.github.com>
2025-11-10 13:19:26 -0500

    d

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -14,7 +14,7 @@ const Index = () => (
           </h2>
           <div className="subtitle">
             <p className="main-subtitle">
-              Junior at NYU Courant
+              Junior at NYU 
             </p>
             <p className="secondary-subtitle">
               Double Majoring in Computer Science & Mathematics <br />
